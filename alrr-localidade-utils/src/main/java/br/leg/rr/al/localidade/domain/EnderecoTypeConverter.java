43c1876cd380413c806a69dbfdbf7b10762e7a18
Ednil Libanio da Costa Junior <ednil.libanio@gmail.com>
2019-03-13 22:55:19 -0400

    funcionalidades migradas para localidade-web-fragment.

--- alrr-localidade-utils/src/main/java/br/leg/rr/al/localidade/domain/EnderecoTypeConverter.java
+++
@@ -1,7 +0,0 @@
-package br.leg.rr.al.localidade.domain;
-
-import br.leg.rr.al.core.utils.EnumAttributeConverter;
-
-public class EnderecoTypeConverter extends EnumAttributeConverter<EnderecoType> {
-
-}
